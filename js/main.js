12e524d416f2268476be011d5288189b8ee98788
Seunghyeok-Ryu <harrisonryu@naver.com>
2022-01-29 15:55:43 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-// Sub-menu
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function (){
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function (){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function (){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
 
 // Scroll 
 // _.throttle(함수, 시간(ms))
@@ -156,6 +139,4 @@ const spyEls = document.querySelectorAll('section.scroll-spy');
       .addTo(new ScrollMagic.Controller())
   });
 
-// YEAR
-const thisYear = document.querySelector('.this-year');
-  thisYear.textContent = new Date().getFullYear();    //2021
+
